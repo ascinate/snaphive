29c84f59d05e34c6554cecf94cf085ca205e0592
Pritam Ghosh <pritam7599@gmail.com>
2025-11-19 13:38:56 +0530

    code

--- src/screen/CreateHive.jsx
+++ src/screen/CreateHive.jsx
@@ -33,6 +33,7 @@ const CreateHive = ({ navigation, route }) => {
     const toggleSwitch = () => setIsEnabled(previousState => !previousState);
     const [uploadType, setUploadType] = useState('automatic');
     const [hiveType, setHiveType] = useState(null);
+const [checked, setChecked] = useState(false);
 
 
     const data = [
@@ -510,6 +511,45 @@ const CreateHive = ({ navigation, route }) => {
                                         </View>
                                     </View>
                                 </View>
+
+
+<View style={{ flexDirection: 'row', alignItems: 'center' }}>
+    <TouchableOpacity
+        onPress={() => setChecked(!checked)}
+        style={{
+            height: 20,
+            width: 20,
+            borderRadius: 4,
+            borderWidth: 2,
+            borderColor: checked ? '#69ec48ff' : '#9CA3AF',
+            alignItems: 'center',
+            justifyContent: 'center',
+            marginRight: 8,
+        }}
+        activeOpacity={0.7}
+    >
+        {checked && (
+            <View
+                style={{
+                    height: 12,
+                    width: 12,
+                    backgroundColor: '#5bec48ff',
+                    borderRadius: 2,
+                }}
+            />
+        )}
+    </TouchableOpacity>
+
+    <View style={{ flexDirection: 'row' }}>
+
+        <CustomText weight="bold" style={{ color: '#000000ff' }}>
+       I agree to the Privacy Policy
+        </CustomText>
+    </View>
+</View>
+
+
+
                                 <View style={{}}>
                                     <ThemeButton
                                         text="Create Hive"
